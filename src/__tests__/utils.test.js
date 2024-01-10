7718a54e722328fb3082041cd56a164c6233faee
Hayden Roney <roneyhayden@gmail.com>
2024-01-10 11:59:12 -0600

    lab done

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,44 @@
 // Your tests here
+import { isPalindrome } from "../utils"
+
+describe("isPalindrome", () => {
+    it("Checks to see if a word is a palindrome", () => {
+        const word = "racecar"
+
+        const palindromeBool = isPalindrome(word)
+
+        expect(palindromeBool).toBe(true)
+    });
+    it("Should return true for words with combinations of Upper and Lower case", () => {
+        const word = "RacEcaR"
+
+        const palindromeBool = isPalindrome(word)
+
+        expect(palindromeBool).toBe(true)
+    });
+    it("Should return false for an empty string", () => {
+        const word = ""
+
+        const palindromeBool = isPalindrome(word)
+
+        expect(palindromeBool).toBe(false)
+    });
+    it("Should throw and error if the word has non-alphabetic characters", () => {
+        const word = "race1car"
+
+        function testWord() {
+            isPalindrome(word)
+        }
+
+        expect(testWord).toThrow(new Error('This word has non-alphabetic characters'))
+    });
+    it("Should throw an error if input isn't a string", () => {
+        const word = true
+
+        function testWord() {
+            isPalindrome(word)
+        }
+
+        expect(testWord).toThrow(new Error('This word is not a string'))
+    });
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,18 @@
 // Your code here
+export function isPalindrome(word) {
+    if (word === "") { return false }
+    if (typeof word !== typeof "string") {
+        throw new Error('This word is not a string')
+    }
+    else if (/^[A-Za-z]+$/.test(word)) {
+        let revereseWord = word.split("").reverse().join("")
+        if (word.toLowerCase() === revereseWord.toLowerCase()) {
+            return true
+        } else {
+            return false
+        }
+    } else {
+        throw new Error('This word has non-alphabetic characters')
+    }
+
+}
